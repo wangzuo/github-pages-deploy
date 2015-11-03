1eb2eadbcda35cb535fd880473f9196ef7140769
Wang Zuo <wzuoadjusted@gmail.com>
2015-11-03 18:21:50 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var
+var shell = require('shelljs');
+
+shell.exec('git branch -f gh-pages');
+shell.exec('git checkout gh-pages');
+shell.exec('git reset --hard master');
+shell.exec('mv gh-pages .publish');
+shell.exec('rm -rf *');
+shell.exec('mv .publish/* .');
+shell.exec('rm -rf .publish');
+shell.exec('git add -A .');
+shell.exec('git commit -a -m "update gh-pages"');
+shell.exec('git checkout master');
+shell.exec('git status');
