91d8846f8186e52260289b729aa15da270e02983
wang zuo <wzuoadjusted@gmail.com>
2015-11-03 22:07:04 +0800

    0.0.2

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var shell = require('shelljs/global');
+#! /usr/bin/env node
 
-cd('.publish');
-exec('git checkout gh-pages');
-exec('git pull');
-exec('git rm -r .');
-exec('cp -R ../gh-pages/* .');
-exec('git add -A .');
-exec('git commit -m "update gh-pages"');
-exec('git push');
+var sh = require('shelljs');
+var packageJson = require('./package.json');
+var config = packageJson['github-pages-deploy'] || {};
+
+sh.config.silent = true;
+
+var src = config.src || 'gh-pages';
+
+sh.exec('mkdir -p .publish');
+sh.cd('.publish');
+var remoteUrl = sh.exec('git config --get remote.origin.url').output.trim();
+
+var dotgit = sh.exec('ls .git');
+if(dotgit.code === 1) sh.exec('git clone ' + remoteUrl + ' .');
+
+
+sh.exec('git checkout -t origin/gh-pages');
+
+sh.exec('git rm -r .');
+sh.exec('cp -R ../' + src + '/* .');
+sh.exec('git add -A .');
+sh.exec('git commit -m "update gh-pages ' + new Date().toISOString() + '"');
+sh.exec('git push');
